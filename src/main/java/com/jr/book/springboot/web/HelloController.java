048b3a2edee401783ed8d0e977634ab5e948695b
bbbnam <bbbnam@naver.com>
2020-07-20 01:23:28 +0900

    aws로 혼자 구현하는 웹 서비스 2장 끝

---
+++ src/main/java/com/jr/book/springboot/web/HelloController.java
@@ -0,0 +1,24 @@
+package com.jr.book.springboot.web;
+
+import com.jr.book.springboot.web.dto.HelloResponseDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name,
+                                     @RequestParam("amount") int amount){
+
+        return new HelloResponseDto(name, amount);
+    }
+
+}
