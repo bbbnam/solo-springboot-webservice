5a076c398ffab112fc8e24ae757632eb4a55f46e
bbbnam <bbbnam@naver.com>
2020-07-28 00:13:33 +0900

    feat : mustache 글 등록 화면 구현

--- src/main/java/com/jr/book/springboot/web/IndexController.java
+++ src/main/java/com/jr/book/springboot/web/IndexController.java
@@ -1,8 +1,10 @@
 package com.jr.book.springboot.web;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor //서비스를 autowired 없이 생성자를 통해 의존성 주입을 해주게 해줌
 @Controller
 public class IndexController {
 
@@ -10,4 +12,11 @@ public class IndexController {
     public String index() {
         return "index";  //뒤의 확장자는 알아서 지원해줌 머스테치 스타터 덕분.. .mustache
     }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
+    }
+
+
 }
