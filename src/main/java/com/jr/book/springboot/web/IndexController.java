58c3a098f754b7767992aad8cd24cb6f5c7fd13d
bbbnam <bbbnam@naver.com>
2020-07-29 23:50:59 +0900

    feat : 조회(목록) 구현

--- src/main/java/com/jr/book/springboot/web/IndexController.java
+++ src/main/java/com/jr/book/springboot/web/IndexController.java
@@ -1,15 +1,21 @@
 package com.jr.book.springboot.web;
 
+import com.jr.book.springboot.services.posts.PostsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RequiredArgsConstructor //서비스를 autowired 없이 생성자를 통해 의존성 주입을 해주게 해줌
 @Controller
 public class IndexController {
 
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";  //뒤의 확장자는 알아서 지원해줌 머스테치 스타터 덕분.. .mustache
     }
 
@@ -17,6 +23,4 @@ public class IndexController {
     public String postsSave() {
         return "posts-save";
     }
-
-
 }
